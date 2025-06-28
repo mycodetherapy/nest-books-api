8d15b4e80e0e91fed9e4ea067482d1987e0c576c
Maksim132 <ovchinnicov132@gmail.com>
2025-06-28 11:14:10 +0300

    add auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/schemas/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...result } = user.toObject();
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async signUp(createUserDto: any): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.usersService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+  }
+}
